47a74db57982e2aa55bcef6be266909d06ad8ec6
mitomtrung98 <101630962+mitomtrung98@users.noreply.github.com>
2022-04-25 11:31:22 +0700

    1

---
+++ dom/dom.js
@@ -0,0 +1,16 @@
+var hihi=document.getElementById('hello')
+var hehe=document.getElementById('hi')
+hihi.className='hoho'
+hehe.className='huhu'
+// var vaaa=hehe.innerHTML
+hehe.innerHTML=`<div class="hia">
+<img src="https://danviet.mediacdn.vn/upload/2-2019/images/2019-05-19/Khi-dot-biet-de-tang-cho-cai-chet-cua-dong-loai-giong-nhu-con-nguoi-khi-1-1558235907-width564height523.png" alt=""></img>
+<p class="p1"> con khi</p>
+</div>
+<div>
+<img src="https://kenh14cdn.com/thumb_w/600/b6e0df09af/2015/09/07/ch1-5226e.jpg" alt=""></img>
+<p class="p1"> con chim</p>
+</div>
+<div>
+<img src="http://thichthucung.com/wp-content/uploads/meo-beo-phi-co-nguy-hiem-khong.jpg" alt=""></img>
+<p class="p1"> con meo</p>`
